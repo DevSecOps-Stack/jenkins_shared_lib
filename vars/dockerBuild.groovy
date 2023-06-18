9d52848d376152a4cd0c400834d7d809b09a5d25
Rakesh <sharma742762@gmail.com>
2023-06-18 20:33:54 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,7 +2,7 @@ def call(String appname, String acrurl, String ImageTag){
     
    sh """
    docker image build -t ${appname} . 
-   docker tag ${appname}:${ImageTag} ${acrurl}/${appname}:${ImageTag}
-   docker tag ${appname}:${ImageTag} ${acrurl}/${appname}:latest
+   docker tag ${appname} ${acrurl}/${appname}:${ImageTag}
+   docker tag ${appname} ${acrurl}/${appname}:${ImageTag}
     """
 }
