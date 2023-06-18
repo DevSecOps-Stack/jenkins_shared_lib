a9bfee92f892976c0398915be7b2c201a7983289
Rakesh <sharma742762@gmail.com>
2023-06-18 19:35:35 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -14,10 +14,3 @@ def call(String project, String ImageTag, String hubUser){
 //      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
 //     """
 // }
-
-def call(String Project, String azure_registry_name){
-   sh """
-      docker build -t ${project} .
-      docker tag ${project}:latest ${azure_registry_name}.azurecr.io/${project}:latest"
-   """
-}
