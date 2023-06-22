9b4de693152b3ffaecf8987763f740fe3bfab63d
Rakesh <sharma742762@gmail.com>
2023-06-23 03:52:03 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,11 @@
-def call(credentialsId){
+//def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
-    }
+//    withSonarQubeEnv(credentialsId: credentialsId) {
+  //       sh 'mvn clean package sonar:sonar'
+  //  }
+//}
+def call(credentialsId){
+withVault(configuration: [timeout: 60, vaultCredentialId: 'vault-token', vaultUrl: 'http://40.121.204.190:8200'], vaultSecrets: [[path: 'secret/sonar', secretValues: [[vaultKey: 'credentialsId']]]]) {
+    sh 'mvn clean package sonar:sonar'
+}
 }
